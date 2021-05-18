cb015c3a3b1ba13394ef43d6bca3ca1791c9cb6e
Marc Becker <marcbecker@posteo.de>
2021-05-18 19:10:00 +0200

    fix nadir

---
+++ install.R
@@ -0,0 +1,4 @@
+install.packages(c("remotes", "mlr3", "mlr3learners"))
+remotes::install_github("mlr-org/bbotk@irace")
+remotes::install_github("mlr-org/paradox@expression_params")
+remotes::install_github("mlr-org/miesmuschel@multiobjective_hb")
--- irace/experiments.R
+++ irace/experiments.R
@@ -4,19 +4,60 @@ folder = "./irace/data"
 set.seed(7345)
 
 # 17.05.2021
-subfolder = "data_17_05_single_b3000"
+subfolder = "test_data_17_05_single_b3000"
 dir.create(file.path(folder,  subfolder))
 
 instances_plan = readRDS(system.file("instances.rds", package = "mfsurrogates"))[test == FALSE & cfg %in% c("rbv2_super", "lcbench")]
 instances_plan[,targets := ifelse(cfg == "lcbench", "val_cross_entropy", "logloss")]
 instances_plan = instances_plan[sample(nrow(instances_plan)), ]
 
+#future::plan("multicore", workers = 40)
+
+res = optimize_irace(
+  instances_plan = instances_plan,
+  evals = 3000,
+  highest_budget_only = TRUE,
+  instance_file = file.path(folder, subfolder, "irace_instance.rda"),
+  log_file = file.path(folder, subfolder, "irace_log.Rdata"),
+  workdir = "./irace/data/surrogates")
+
+# 17.05.2021
+# small_test
+subfolder = "test_data_17_05_single_b3000"
+dir.create(file.path(folder,  subfolder))
+
+instances_plan = readRDS(system.file("instances.rds", package = "mfsurrogates"))[test == FALSE & cfg %in% c("rbv2_super", "lcbench")]
+instances_plan[,targets := ifelse(cfg == "lcbench", "val_cross_entropy", "logloss")]
+instances_plan = instances_plan[sample(nrow(instances_plan)), ]
+
+# future::plan("multicore", workers = 8)
+
+res = optimize_irace(
+  instances_plan = instances_plan,
+  evals = 300,
+  highest_budget_only = TRUE,
+  instance_file = file.path(folder, subfolder, "irace_instance.rda"),
+  log_file = file.path(folder, subfolder, "irace_log.Rdata"),
+  workdir = "./irace/data/surrogates")
+
+# 17.05.2021
+# small_test_multi
+subfolder = "test_data_17_05_multi_b3000"
+dir.create(file.path(folder,  subfolder))
+
+instances_plan = readRDS(system.file("instances.rds", package = "mfsurrogates"))[test == FALSE & cfg %in% c("rbv2_super", "lcbench")]
+instances_plan[,targets := ifelse(cfg == "lcbench", list(c("val_cross_entropy", "time")), list(c("logloss", "timepredict")))]
+instances_plan = instances_plan[c(1, 9), ]
+
 future::plan("multicore", workers = 40)
 
 res = optimize_irace(
   instances_plan = instances_plan,
   evals = 3000,
+  highest_budget_only = TRUE,
   instance_file = file.path(folder, subfolder, "irace_instance.rda"),
-  log_file = file.path(folder, subfolder, "irace_log.Rdata"))
+  log_file = file.path(folder, subfolder, "irace_log.Rdata"),
+  workdir = "./irace/data/surrogates")
+
 
 
