b75ab20c808677b5aa4fa033e1d766b005131231
juliambr <ju.moosbauer@googlemail.com>
2021-07-19 12:22:53 +0200

    reduce budget for hpbster

--- experiments/submit.R
+++ experiments/submit.R
@@ -14,7 +14,7 @@ tab = summarizeExperiments(by = c("job.id", "problem", "task", "algorithm", "alg
 # Testing every version of algorithm / problem with full budget
 # tasks = c("126026", "126029", "189908", "7593")
 
-tosubmit = tab[problem == "lcbench", ]
+tosubmit = tab[problem == "rbv2_super", ]
 tosubmit = ijoin(tosubmit, findNotDone())
 tosubmit = tosubmit[- which(job.id %in% findOnSystem()$job.id), ]
 
@@ -22,23 +22,17 @@ tosubmit = tosubmit[- which(job.id %in% findOnSystem()$job.id), ]
 # 1. RANDOMSERACH (Budget factor 32)
 tosubmit_rs = tosubmit[algorithm == "randomsearch_full_budget", ]
 tosubmit_rs$chunk = chunk(tosubmit_rs$job.id, chunk.size = 15)
-
 submitJobs(tosubmit_rs, resources = resources.serial.default)
 
 
 # 2. mlrintermbo (NOT SUBMITTED YET)
-# Time: ~ X minutes (lcbench)
-#       ~ X minutes (rbv2_super)
 tosubmit_mbo = tosubmit[algorithm == "mlrintermbo_full_budget", ]
 tosubmit_mbo$chunk = chunk(tosubmit_mbo$job.id, chunk.size = 50)
 
 submitJobs(tosubmit_mbo, resources = resources.serial.default)
 
 
-
 # 3. BOHB
-# Time: ~ 1.3 minutes (lcbench)
-#       ~ 20 minutes (rbv2_super)
 tosubmit_hpbster = tosubmit[algorithm == "hpbster_bohb", ]
 tosubmit_hpbster$chunk = chunk(tosubmit_hpbster$job.id, chunk.size = 50)
 tosubmit_hpbster = tosubmit_hpbster[- which(job.id %in% findOnSystem()$job.id), ]
@@ -47,8 +41,6 @@ submitJobs(tosubmit_hpbster, resources = resources.serial.default)
 
 
 # 4. HB
-# Time: ~ 0.5 minutes (lcbench)
-#       ~ 3 minutes (rbv2_super)
 tosubmit_hpbster = tosubmit[algorithm == "hpbster_hb", ]
 tosubmit_hpbster$chunk = chunk(tosubmit_hpbster$job.id, chunk.size = 350)
 tosubmit_hpbster = tosubmit_hpbster[- which(job.id %in% findOnSystem()$job.id), ]
@@ -56,8 +48,6 @@ submitJobs(tosubmit_hpbster, resources = resources.serial.default)
 
 
 # 5. mlr3hyperband (Budget factor 32)
-# Time: ~ 5 minutes (lcbench)
-#       ~ 90 minutes (rbv2_super)
 tosubmit_hb = tosubmit[algorithm == "mlr3hyperband", ]
 tosubmit_hb$chunk = chunk(tosubmit_hb$job.id, chunk.size = 20)
 tosubmit_hb = tosubmit_hb[- which(job.id %in% findOnSystem()$job.id), ]
@@ -65,8 +55,6 @@ submitJobs(tosubmit_hb, resources = resources.serial.default)
 
 
 # 6. smac
-# Time: ~ 5 minutes (lcbench)
-#       ~ 120 minutes (rbv2_super)
 tosubmit_smac = tosubmit[algorithm == "smac_full_budget", ] # multi.point = 1 only uses 1/32 of the budget than when it is run with multi.point NA
 tosubmit_smac$chunk = chunk(tosubmit_smac$job.id, chunk.size = 6)
 tosubmit_smac = tosubmit_smac[- which(job.id %in% findOnSystem()$job.id), ]
@@ -74,40 +62,29 @@ submitJobs(tosubmit_smac, resources = resources.serial.default)
 
 
 # 7. smac hb
-# Time: ~ X minutes (lcbench)
-#       ~ X minutes (rbv2_super)
 tosubmit_smac = tosubmit[algorithm == "smac_hb", ] # multi.point = 1 only uses 1/32 of the budget than when it is run with multi.point NA
 tosubmit_smac$chunk = batchtools::chunk(tosubmit_smac$job.id, chunk.size = 100)
-tosubmit_smac = tosubmit_smac[- which(job.id %in% findOnSystem()$job.id), ]
-submitJobs(tosubmit_smac[chunk == 1, ], resources = resources.serial.default)
-
-
-# 7. smac bohb
-# Time: ~ 5 minutes (lcbench)
-#       ~ 120 minutes (rbv2_super)
-tosubmit_smac = tosubmit[algorithm == "smac_bohb", ] # multi.point = 1 only uses 1/32 of the budget than when it is run with multi.point NA
-tosubmit_smac$chunk = chunk(tosubmit_smac$job.id, chunk.size = 6)
 tosubmit_smac = tosubmit_smac[- which(job.id %in% findOnSystem()$job.id), ]
 submitJobs(tosubmit_smac, resources = resources.serial.default)
 
 
-# 9. Focussearch (NOT SUBMITTED YET )
-# Time: ~ X minutes 
+# 7. smac bohb
+tosubmit_smac = tosubmit[algorithm == "smac_bohb", ] # multi.point = 1 only uses 1/32 of the budget than when it is run with multi.point NA
+tosubmit_smac$chunk = batchtools::chunk(tosubmit_smac$job.id, chunk.size = 20)
+tosubmit_smac = tosubmit_smac[- which(job.id %in% findOnSystem()$job.id), ]
+submitJobs(tosubmit_smac, resources = resources.serial.default)
+
+
+# 9. Focussearch 
 tosubmit_fs = tosubmit[algorithm == "focussearch_full_budget", ] # multi.point = 1 only uses 1/32 of the budget than when it is run with multi.point NA
-tosubmit_fs$chunk = chunk(tosubmit_fs$job.id, chunk.size = 5)
+tosubmit_fs$chunk = batchtools::chunk(tosubmit_fs$job.id, chunk.size = 1050)
 tosubmit_fs = tosubmit_fs[- which(job.id %in% findOnSystem()$job.id), ]
 submitJobs(tosubmit_fs, resources = resources.serial.default)
 
 
-# Check whether those first jobs are done
-table(ijoin(tab, findDone())[task %in% tasks, ]$algorithm)
-table(ijoin(tab, findSubmitted())[task %in% tasks, ]$algorithm)
 
 
-
-
-
-# STATUS 13.07.2021
+# STATUS 19.07.2021
 
 ## SEQUENTIAL EXPERIMENTS
 
@@ -137,14 +114,14 @@ table(ijoin(tab, findSubmitted())[task %in% tasks, ]$algorithm)
 
 ### - Registry: reg_sequential (LRZ)
 ### - Test run (to get the time): 
-###		- randomsearch_full_budget: 12391; 90 minutes 
-###		- hpbster_hb: 3 Minutes 
 ###		- hpbster_bohb: 20 Minutes 
 ###		- smac_full_budget: 120 minutes
 ### - Completed: 
-###		- hpbster_hb
-###		- hpbster_bohb
+###		- randomsearch_full_budget: 98 minutes 
 ### - Submitted: 
+###		- hpbster_hb: 300 minutes 
+###		- hpbster_bohb
+
 ### 	- randomsearch_full_budget
 ###		- mlr3hyperband 
 ### - Not submitted:  
@@ -165,10 +142,11 @@ table(ijoin(tab, findSubmitted())[task %in% tasks, ]$algorithm)
 ###		- hpbster_bohb: 1.3 minutes
 ###		- smac_full_budget: 5.9 minutes 
 ###		- mlr3hyperband: 4.8 minutes 
-### - Submitted: 
-### - Not submitted:  
 ###		- smac_hb: 1.4 minutes 
 ###		- smac_bohb: 15 minutes 
+###		- focussearch_full_budget: 0.25 minutes
+### - Submitted: 
+### - Not submitted:  
 ###   - mlrintermbo_full_budget
 ###   - random_search
 ###   - smac
@@ -183,52 +161,6 @@ table(ijoin(tab, findSubmitted())[task %in% tasks, ]$algorithm)
 # mlrintermbo (full_budget = FALSE): 	34 min
 # mlr3hyperband:  						7 sec
 # hpbster:		 						22 sec
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Visualize the outcome 
-library(batchtools)
-
-res = reduceResultsDataTable()
-res = ijoin(tab, res)
-
-# Draw learning curves (per task per problem averaged over iterations)
-dfs = lapply(seq_len(nrow(res)), function(i) {
-	out = cbind(res[i, - c("result")], res[i, ]$result[[1]]$archive$data[, c("epoch", "val_accuracy")])
-	out$y_cum = cummax(out$val_accuracy)
-
-	if (out$algorithm == "smashy") {
-		out$epoch = exp(out$epoch)
-	}
-
-	out$budget_cum = cumsum(out$epoch)
-
-	out
-})
-
-df = do.call(rbind, dfs)
-
-df$algorithm_variant = paste0(df$algorithm, ifelse(!df$full_budget | is.na(df$full_budget), "", "_fullbudget"))
-
-
-library(ggplot2)
-
-p = ggplot(data = df, aes(x = log(budget_cum), y = y_cum, colour = algorithm_variant, group = job.id)) + geom_line() 
-p
-
 
 
 
